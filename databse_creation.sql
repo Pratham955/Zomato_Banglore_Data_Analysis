14e36e6effcdc40c46abe3358ed4b3f5c453518a
Pratham Singh <75075887+Pratham955@users.noreply.github.com>
2024-01-10 23:36:04 +0530

    Create databse_creation.sql

---
+++ databse_creation.sql
@@ -0,0 +1,12 @@
+CREATE TABLE sales(
+   restaurant_name VARCHAR(300),
+   restaurant_type VARCHAR(30),
+   rating VARCHAR(5),
+   num_of_ratings INTEGER,
+   avg_cost_two_people VARCHAR(5), 
+   online_order VARCHAR(5),
+   table_booking VARCHAR(5),
+   cuisines_type VARCHAR(83),
+   area VARCHAR(30),
+   local_address VARCHAR(30) 
+);
